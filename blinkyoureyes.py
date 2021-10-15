be6e61d92c2b86de5287203bdafdcf4b6acd8ccc
Shohei Fujii <fujii.shohei@gmail.com>
2021-10-15 18:48:10 +0800

    now resizable

--- blinkyoureyes.py
+++ blinkyoureyes.py
@@ -6,6 +6,14 @@ __author__ = "ompugao"
 import sys
 from PyQt5 import QtCore, QtWidgets, QtGui
 
+from enum import Enum
+
+class DragMode(Enum):
+    MOVE = 1
+    RESIZEX = 2
+    RESIZEY = 3
+    RESIZEDIAG = 4
+
 class BlinkYourEyesWidget(QtWidgets.QWidget):
 
     def __init__(self, parent = None, widget = None):
@@ -21,43 +29,79 @@ class BlinkYourEyesWidget(QtWidgets.QWidget):
         self.background_color = QtCore.Qt.black
         self.timer_count = 0
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(100) #[milliseconds]
+        self.timer.setInterval(100)  # [milliseconds]
         self.timer.timeout.connect(self.timer_callback)
 
         self.initUI()
         self.timer.start()
+
+        self.dragmode = None
         self.dragstartpos = None
+        self.dragstartgeom = None
 
     def initUI(self):
         screenrect = QtWidgets.QDesktopWidget().screenGeometry().getRect()
-        width = int(screenrect[2] / 8) #170
-        height = int(screenrect[3] / 8) #80
-        self.setGeometry(screenrect[2] - width, 0, width, height) #screenrect[3] - height
+        width = int(screenrect[2] / 8)  # 170
+        height = int(screenrect[3] / 8)  # 80
+        self.setGeometry(screenrect[2] - width, screenrect[3] - height, width, height)  # screenrect[3] - height or 0
         self.setWindowTitle('Blink Your Eyes')
         self.show()
 
     def mousePressEvent(self, event):
         if event.buttons() & QtCore.Qt.LeftButton:
-            self.dragstartpos = event.pos()
-            self.xy = [event.x, event.y]
-            self.setCursor(QtGui.QCursor(QtCore.Qt.SizeAllCursor))
+            geom = self.geometry()
+            self.dragstartpos = event.pos()  # in window
+            self.dragstartgeom = self.geometry()
+
+            on_edge_x = geom.width()*3/4 < event.x() < geom.width()
+            on_edge_y = geom.height()*3/4 < event.y() < geom.height()
+            if on_edge_x and on_edge_y:
+                self.dragmode = DragMode.RESIZEDIAG
+                self.setCursor(QtGui.QCursor(QtCore.Qt.SizeFDiagCursor))
+            elif on_edge_x and not on_edge_y:
+                self.dragmode = DragMode.RESIZEX
+                self.setCursor(QtGui.QCursor(QtCore.Qt.SizeHorCursor))
+            elif not on_edge_x and on_edge_y:
+                self.dragmode = DragMode.RESIZEY
+                self.setCursor(QtGui.QCursor(QtCore.Qt.SizeVerCursor))
+            else:
+                self.dragmode = DragMode.MOVE
+                self.setCursor(QtGui.QCursor(QtCore.Qt.SizeAllCursor))
 
     def mouseMoveEvent(self, event):
-        #self.move(event.x - self.xy[0], event.y - self.xy[1])
         if (self.dragstartpos is not None\
                 and event.buttons() & QtCore.Qt.LeftButton):
                 #and (event.pos() - self.dragstartpos).manhattanLength() > QtWidgets.qApp.startDragDistance()):
-            event.pos() - self.dragstartpos
-            pos = QtCore.QPoint(event.globalPos())
-            self.window().move(pos - self.dragstartpos)
+
+            geom = self.dragstartgeom
+            pointerpos = QtCore.QPoint(event.globalPos())
+            neworiginpos = pointerpos - self.dragstartpos
+            diff = event.pos() - self.dragstartpos
+            minsize = 100
+            newwidth = max(minsize, geom.width() + diff.x())
+            newheight = max(minsize, geom.height() + diff.y())
+            # print('----')
+            # print(self.dragstartpos)
+            # print(diff)
+            # print(neworiginpos.x(), neworiginpos.y())
+            if self.dragmode is DragMode.RESIZEDIAG:
+                self.setGeometry(geom.x(), geom.y(), newwidth, newheight)
+            elif self.dragmode is DragMode.RESIZEX:
+                self.setGeometry(geom.x(), geom.y(), newwidth, geom.height())
+            elif self.dragmode is DragMode.RESIZEY:
+                self.setGeometry(geom.x(), geom.y(), geom.width(), newheight)
+            else:
+                # self.dragmode = DragMode.MOVE
+                self.window().move(neworiginpos)
             #self.dragstart = None
             #drag = QtGui.QDrag(self)
             #drag.setMimeData(QtCore.QMimeData())
             #drag.exec_(QtCore.Qt.LinkAction)
 
-
     def mouseReleaseEvent(self, event):
         self.dragstartpos = None
+        self.dragmode = None
+        self.dragstartgeom = None
         self.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
 
     def timer_callback(self, ):
